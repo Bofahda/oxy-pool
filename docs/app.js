d69bd37542412ea6fe90aa0d485bf315809ebab2
Jan <lepetitjan@users.noreply.github.com>
2017-10-18 00:15:48 +0200

    Added some properties

--- docs/app.js
+++ docs/app.js
@@ -1,13 +1,19 @@
 var app = angular.module('delegateApp', []);
 
 app.controller('indexCtrl', function($scope, $http) {
+    $scope.title = "OXYCOIN Official Pool";
+    $scope.sharing = 90;
+    $scope.schedule = "day";
+    $scope.scheduleLong = "daily";
+    $scope.username = "official_pool";
+
     $scope.accounts = [];
     $scope.lastpayout = 0;
     $scope.nextpayout = 0;
 
     $http.get ('poollogs.json').then (function (res) {
         $scope.lastpayout = res.data.lastpayout * 1000;
-        $scope.nextpayout = moment ($scope.lastpayout).add (1, 'week').valueOf();
+        $scope.nextpayout = moment ($scope.lastpayout).add (1, $scope.schedule).valueOf();
         $scope.accounts = [];
 
         for (addr in res.data.accounts) {
@@ -17,7 +23,7 @@ app.controller('indexCtrl', function($scope, $http) {
         }
     });
 
-    $http.get ('https://wallet.oxycoin.io/api/delegates/get?username=jan').then (function (res) {
+    $http.get ('https://wallet.oxycoin.io/api/delegates/get?username='+$scope.username).then (function (res) {
         $scope.delegate = res.data.delegate;
     });
 });
